5b13a3924a7ba6370fe09b89de7abcc73e615b7c
ItxSamin <samin8a@gmail.com>
2025-07-26 15:32:36 +0500

    added a pages

---
+++ index.php
@@ -0,0 +1,44 @@
+    <?php include('includes/header.php');?>
+      <div class="container">
+        <div class="row">
+            <div class="col-md-12 mt-5" >
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Student_Results</h4>
+                        <a href="register.php" class="btn btn-primary float-right">Register/Add</a>
+                    </div>
+                    <div class="card-body">
+                        
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                <th scope="col">Roll_num</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Semester</th>
+                                <th scope="col">Total-marks</th>
+                                <th scope="col">Obtained Marks</th>
+                                <th scope="col">Edit</th>
+                                <th scope="col">Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <th>6</th>
+                                <td>Hassam</td>
+                                <td>4th</td>
+                                <td>150</td>
+                                <td>130</td>
+                                <td><a href="" class="btn btn-info">Edit</a></td>
+                                <td>
+                                    <a href="" class="btn btn-danger">Danger</a>
+                                </td>
+                                </tr>
+                            </tbody>
+                            </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include('includes/header.php');?>
---
+++ views/edit_form.php
@@ -0,0 +1,48 @@
+    <?php 
+    include("../includes/header.php");
+    
+    
+    ?>
+
+
+<div class="container">
+    <div class="row" style="margin-top: 10vh;">
+        <div class="col-md-10">
+            <div class="card" style="padding: 20px;">
+                <div class="card-header">
+                    <h4>Result Enter here!</h4>
+                </div>
+                <div class="card-body">
+                    <form style="margin-top: 20px;margin-right: 30vh;">
+                            <div class="mb-3">
+                                <label>Name</label>
+                                <input type="text" name ="name" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label>Semester</label>
+                                <input type="text" name ="semester" class="form-control">
+                            </div><div class="mb-3">
+                                <label>Total_Marks</label>
+                                <input type="text" name ="tmarks" class="form-control">
+                            </div><div class="mb-3">
+                                <label>Obtained_Marks</label>
+                                <input type="text" name ="omarks" class="form-control">
+                            
+                            </div>
+                            <div class="mb-3">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Update</button>
+                        </fieldset>
+                        </form>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+    <?php include('../includes/footer.php');?>
